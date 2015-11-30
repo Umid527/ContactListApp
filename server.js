6c9d0934537bc3bbbd3f99e032d478f800fff523
Ümid <umid.ma@gmail.com>
2015-11-30 18:52:56 +0400

    contactlist Dummy - 1

--- server.js
+++ server.js
@@ -4,5 +4,9 @@ var app=express();
 //	res.send("Hello world from server.js ");
 //});
 app.use(express.static(__dirname+"/public"));
+app.get('/contactList',function (req,res){
+	console.log("I received a GET request ");
+});
+
 app.listen(3000);
 console.log("Server running on port 3000	");
