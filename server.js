f03f0fc1aeebca4297d8a88eb4e842db26801702
Ümid <umid.ma@gmail.com>
2015-11-30 19:02:35 +0400

    contactlist Dummy - 2

--- server.js
+++ server.js
@@ -4,8 +4,25 @@ var app=express();
 //	res.send("Hello world from server.js ");
 //});
 app.use(express.static(__dirname+"/public"));
-app.get('/contactList',function (req,res){
+app.get('/contactlist',function (req,res){
 	console.log("I received a GET request ");
+	   person1 = {
+    	name: 'Tim',
+    	email: 'tim@email.com',
+    	number: '(111) 111-1111'
+    };
+    person2 = {
+    	name: 'Emily',
+    	email: 'emily@email2.com',
+    	number: '(222) 222-2222'
+    };
+    person3 = {
+    	name: 'John',
+    	email: 'john@email3.com',
+    	number: '(333) 333-3333'
+    };
+    var contactlist=[person1,person2,person3];
+    res.json(contactlist);
 });
 
 app.listen(3000);
